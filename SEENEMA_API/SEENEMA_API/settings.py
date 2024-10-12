649479d7e47b20b8ffea0469543fd6ed709b94f7
vtbossss <tiwarijivaibhav@gmail.com>
2024-10-12 14:20:34 +0000

    envresolved

--- SEENEMA_API/SEENEMA_API/settings.py
+++ SEENEMA_API/SEENEMA_API/settings.py
@@ -31,9 +31,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['seenema.pythonanywhere.com']
+
 
 
 # Application definition
@@ -49,7 +50,7 @@ INSTALLED_APPS = [
     "rest_framework_simplejwt",
     "api",
     "drf_spectacular",
-    
+
 ]
 
 MIDDLEWARE = [
